264d3e6a8b49fc25c8b26bdf75e518f20c99785d
Miras <gabrielznrg@gmail.com>
2026-08-20 14:34:33 -0300

    Sem poder Rodar

--- Aula 1/exemplo1.c
+++ Aula 1/exemplo1.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+use
 
-int main(void)
-{
-    printf("Pause\n");
-    system("pause");
-    
+int main() {
+    float nota1, nota2, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    printf("Sua media e: %.2f\n", media);
+
+    if (media >= 6.0) {
+        printf("Status: Aprovado!\n");
+    } else {
+        printf("Status: Reprovado!\n");
+    }
+
     return 0;
 }
