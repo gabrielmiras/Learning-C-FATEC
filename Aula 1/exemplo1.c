64ce9d81f31784d1677cc7b6a84fe9f35fce4a52
GABRIEL ZIFIRINO DE MIRANDA <gabrielznrg@gmail.com>
2026-08-27 08:20:10 -0300

    Bibliotecas C

--- Aula 1/exemplo1.c
+++ Aula 1/exemplo1.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-use
 
 int main() {
     float nota1, nota2, media;
 
     printf("Digite a primeira nota: ");
-    scanf("%f", &nota1);
+    scanf(" %f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", &nota2);
+    scanf(" %f", &nota2);
 
     media = (nota1 + nota2) / 2;
 
